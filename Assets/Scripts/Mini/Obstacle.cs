ad623856de03496bbb1ef421a9a8c9d6e6f04086
100ZADA <5528asd@gmail.com>
2025-02-18 11:18:12 +0900

    [Feat] Obstacle
    Obstacle 코드 추가 및 생성 동작 진행
    FollowCamera.cs 생성

--- Assets/Scripts/Mini/Obstacle.cs
+++ Assets/Scripts/Mini/Obstacle.cs
@@ -6,14 +6,31 @@ using Random = UnityEngine.Random;
 public class Obstacle : MonoBehaviour
 {
     // 장애물 위치
-    public float higPosY = 1f;
-    public float lowPosX = -1f;
+    public float highPosY = 1f;
+    public float lowPosY = -1f;
 
     // 장애물 사이즈
     public float holeSizeMin = 1f;
     public float holeSizeMax = -1f;
 
-    //
     public Transform topObject;
     public Transform bottomObject;
+
+    public float widthPadding = 4f;
+
+    // 장애물 랜덤 소환
+    public Vector3 SetRandomPlace(Vector3 lastPosition, int obstacleCount)
+    {
+        float holeSize = Random.Range(holeSizeMin, holeSizeMax);
+        float halfHoleSize = holeSize / 2f;
+        topObject.localPosition = new Vector3(0, halfHoleSize);
+        bottomObject.localPosition = new Vector3(0, -holeSize);
+
+        Vector3 placePosition = lastPosition + new Vector3(widthPadding, 0);
+        placePosition.y = Random.Range(lowPosY, highPosY);
+
+        transform.position = placePosition;
+
+        return placePosition;
+    }
 }
