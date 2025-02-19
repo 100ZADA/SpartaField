66e7373716c2ae8f13050ec98966965655fda507
100ZADA <5528asd@gmail.com>
2025-02-19 10:16:37 +0900

    [Fest] TransScene
    TransScene을 통해 특정 구역 이동시 Scene 전환

---
+++ Assets/Scripts/Entity/TransferScene.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransferScene : MonoBehaviour
+{
+    // 이동할 맵 이름
+    public string transferMapName;
+
+    void Start()
+    {
+     
+    }
+
+    // 플레이어와 충돌 시
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(transferMapName);
+        }
+    }
+}
